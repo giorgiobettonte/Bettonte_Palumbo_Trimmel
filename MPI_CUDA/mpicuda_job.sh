ad435c7a7b336cb875697081173d5238dfa9dfb2
giorgiobettonte <151047684+giorgiobettonte@users.noreply.github.com>
2024-03-07 18:43:35 +0100

    Update mpicuda_job.sh

--- MPI_CUDA/mpicuda_job.sh
+++ MPI_CUDA/mpicuda_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#SBATCH --nodes=2                          # number of nodes
+#SBATCH --nodes=1                          # number of nodes
 #SBATCH --ntasks=4                      # number of tasks
-#SBATCH --ntasks-per-node=2               # number of tasks per node
+#SBATCH --ntasks-per-node=4               # number of tasks per node
 #SBATCH --gpus-per-task=1                  # number of gpu per task
 #SBATCH --cpus-per-task=1                  # number of cores per task
 #SBATCH --time=00:15:00                    # time (HH:MM:SS)
