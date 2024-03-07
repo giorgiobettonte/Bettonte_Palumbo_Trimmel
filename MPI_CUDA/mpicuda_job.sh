399c5e019b48f7e399b3f7950f4dfdcb7854b641
giorgiobettonte <151047684+giorgiobettonte@users.noreply.github.com>
2024-03-07 18:10:06 +0100

    Update mpicuda_job.sh

--- MPI_CUDA/mpicuda_job.sh
+++ MPI_CUDA/mpicuda_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1                  # number of cores per task
 #SBATCH --time=00:15:00                    # time (HH:MM:SS)
 #SBATCH --partition=gpu                    # partition
-#SBATCH --account=p200301                 # project account
+#SBATCH --account=account                # project account
 #SBATCH --qos=default                      # SLURM qos
 
 
